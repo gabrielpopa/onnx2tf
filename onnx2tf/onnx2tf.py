20733e70be152ca0f07dcc3f4b89645b2bfd4205
pinto0309 <rmsdh122@yahoo.co.jp>
2024-07-11 09:12:57 +0900

    Add type hints

--- onnx2tf/onnx2tf.py
+++ onnx2tf/onnx2tf.py
@@ -1621,7 +1621,6 @@ def convert(
         # Quantized TFLite
         if output_integer_quantized_tflite:
             # Get signatures/input keys
-
             trackable_obj = \
                 tf.saved_model.load(
                     output_folder_path

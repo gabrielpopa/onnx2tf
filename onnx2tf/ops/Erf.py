c4ef2599b67ec3210cfa482971c8ca64d678de4f
Gabriel Popa <me@gabrielpopa.com>
2024-10-21 22:01:14 +0200

    Fix pseudo Erf to run on the GPU delegate

--- onnx2tf/ops/Erf.py
+++ onnx2tf/ops/Erf.py
@@ -13,7 +13,7 @@ from onnx2tf.utils.common_functions import (
     pre_process_transpose,
     post_process_transpose,
 )
-
+from onnx2tf.utils.logging import *
 
 @print_node_info
 @inverted_operation_enable_disable
@@ -92,19 +92,53 @@ def make_node(
                     name=graph_node.name,
                 )
         else:
+            warn("(gp) Pseudo Erf")
             # https://stackoverflow.com/questions/457408/is-there-an-easily-available-implementation-of-erf-for-python
-            eps = 1e-11
             x_abs = tf.math.abs(input_tensor)
-            sign = tf.math.divide(input_tensor, tf.math.abs(input_tensor)+eps)
-            a1 =  0.254829592
-            a2 = -0.284496736
-            a3 =  1.421413741
-            a4 = -1.453152027
-            a5 =  1.061405429
-            p  =  0.3275911
-            t = 1.0/(1.0 + p*x_abs)
-            y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*tf.math.exp(-x_abs*x_abs)
-            erf_tensor = sign*y
+            sign = tf.sign(input_tensor)
+
+            # (gp) Working
+            # a1 =  0.254829592
+            # a2 = -0.284496736
+            # a3 =  1.421413741
+            # a4 = -1.453152027
+            # a5 =  1.061405429
+            # p  =  0.3275911
+            # t = tf.math.divide(1.0,1.0 + p*x_abs)
+            # y = 1.0 - (((((a5*t + a4)*t) + a3)*t + a2)*t + a1)*t*tf.math.exp(-x_abs*x_abs)
+            # erf_tensor = sign*y
+
+            # (gp) Suggested to work
+            a1 = tf.constant(0.254829592, dtype=input_tensor.dtype)
+            a2 = tf.constant(-0.284496736, dtype=input_tensor.dtype)
+            a3 = tf.constant(1.421413741, dtype=input_tensor.dtype)
+            a4 = tf.constant(-1.453152027, dtype=input_tensor.dtype)
+            a5 = tf.constant(1.061405429, dtype=input_tensor.dtype)
+            p  = tf.constant(0.3275911, dtype=input_tensor.dtype)
+            t = tf.math.divide(1.0, tf.math.add(1.0, tf.math.multiply(p, x_abs)))
+            y = tf.math.subtract(
+                1.0, 
+                tf.math.multiply(
+                    (((((tf.math.multiply(a5, t) + a4) * t) + a3) * t) + a2) * t + a1,
+                    t
+                ) * tf.math.exp(-tf.math.multiply(x_abs, x_abs))
+            )
+            erf_tensor = tf.math.multiply(sign, y)
+
+            # (gp) Coefficients for the Abramowitz and Stegun approximation
+            # t = tf.math.divide(1.0, (1.0 + 0.5 * x_abs))
+            # y = 1.0 - t * tf.math.exp(-x_abs*x_abs - 1.26551223 + 
+            #         t * (1.00002368 + 
+            #         t * (0.37409196 + 
+            #         t * (0.09678418 + 
+            #         t * (-0.18628806 + 
+            #         t * (0.27886807 + 
+            #         t * (-1.13520398 + 
+            #         t * (1.48851587 + 
+            #         t * (-0.82215223 + 
+            #         t * 0.17087277)))))))))
+            # erf_tensor = sign * y
+
             tf_layers_dict[graph_node_output.name]['tf_node'] = erf_tensor
         tf_type = tf.math.erf
     else:
